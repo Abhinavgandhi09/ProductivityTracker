0e7fb304125aa74f6ea2a51424634a3f401fa975
Fadi Alladkani <fmalladkani@wpi.edu>
2020-02-21 12:24:31 -0500

    Added python installation/better time

--- installCatkin.sh
+++ installCatkin.sh
@@ -1,1 +1,3 @@
-echo "alias \"c={ time catkin_make ; } 2>&1 | tee ~/.catkin_time && date >> ~/.catkin_time && hostname >> ~/.catkin_time\"" >> ~/.bashrc
+sudo ln -s "$(pwd)/parse.py" "/usr/local/bin/parse_catkin_time"
+chmod +x "$(pwd)/parse.py"
+echo "alias \"c={ time catkin_make ; } 2>&1 | tee ~/.catkin_time && { echo \\\"DATE:\\\" ; date ; } >> ~/.catkin_time && { echo \\\"HOST:\\\" ; hostname ; } >> ~/.catkin_time\"" >> ~/.bashrc
