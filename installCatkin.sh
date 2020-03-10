758cee115e5ac60f8d1dab629606bee56fce9d2f
Fadi Alladkani <fmalladkani@wpi.edu>
2020-03-10 14:03:47 -0400

    switched to UNIX timestamp

--- installCatkin.sh
+++ installCatkin.sh
@@ -1,3 +1,3 @@
 sudo ln -s "$(pwd)/parse.py" "/usr/local/bin/parse_catkin_time"
 chmod +x "$(pwd)/parse.py"
-echo "alias \"c={ time catkin_make ; } 2>&1 | tee ~/.catkin_time && { echo \\\"DATE:\\\" ; date ; } >> ~/.catkin_time && { echo \\\"HOST:\\\" ; hostname ; } >> ~/.catkin_time\"" >> ~/.bashrc
+echo "alias \"c={ time catkin_make ; } 2>&1 | tee ~/.catkin_time && { echo \\\"DATE:\\\" ; { date +%s} ; } >> ~/.catkin_time && { echo \\\"HOST:\\\" ; hostname ; } >> ~/.catkin_time\"" >> ~/.bashrc
